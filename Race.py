a75cddeb00c47572b57fb8142c99e5c8c8b4c246
Oláh Tamás <tolah94@gmail.com>
2018-11-27 13:03:34 +0100

    Implemented Race simulation

--- Race.py
+++ Race.py
@@ -3,20 +3,48 @@ from com.codecool.vehicles.Motorcycle import Motorcycle
 from com.codecool.vehicles.Truck import Truck
 from com.codecool.weather.Weather import Weather
 
-car = Car()
-car2 = Car()
-car.move_for_an_hour()
-car2.move_for_an_hour()
-car2.move_for_an_hour()
-print(car)
-print(car2)
-motor = Motorcycle()
-motor2 = Motorcycle()
-for i in range(3):
-    motor.move_for_an_hour()
-print(motor)
-print(motor2)
-truck = Truck()
-truck.move_for_an_hour()
-print(truck)
+
+class Race:
+
+    __NUMBER_OF_CARS = 10
+    __NUMBER_OF_MOTORCYCLES = 10
+    __NUMBER_OF_TRUCKS = 10
+    __RACE_HOURS = 50
+
+    def __init__(self):
+        self.vehicles = []
+
+    def create_vehicles(self):
+        for i in range(Race.__NUMBER_OF_CARS):
+            self.vehicles.append(Car())
+        for i in range(Race.__NUMBER_OF_MOTORCYCLES):
+            self.vehicles.append(Motorcycle())
+        for i in range(Race.__NUMBER_OF_TRUCKS):
+            self.vehicles.append(Truck())
+
+    def simulate_race(self):
+        for i in range(Race.__RACE_HOURS):
+            Weather.set_raining()
+            for j in range(len(self.vehicles)):
+                self.vehicles[j].move_for_an_hour()
+
+    def print_race_results(self):
+        for i in range(len(self.vehicles)):
+            if i == 0 or self.vehicles[i].__class__.__name__ != vehicle_type:
+                vehicle_type = self.vehicles[i].__class__.__name__
+                print("***** " + vehicle_type + "s *****")
+            print(self.vehicles[i])
+
+    def is_there_a_broken_truck(self):
+        for i in range(len(self.vehicles)):
+            if self.vehicles[i].__class__.__name__ == "Truck" and self.vehicles[i].break_down_turns_left > 0:
+                return True
+        return False
+
+
+if __name__ == "__main__":
+    race = Race()
+    race.create_vehicles()
+    race.simulate_race()
+    race.print_race_results()
 
